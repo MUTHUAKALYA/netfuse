4c52e89a86aa8a335a6c94c706572e9b32287a84
MUTHUAKALYA <muthuakalyaa@gmail.com>
2024-07-27 17:29:53 +0530

    responsive home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <main className="p-10 flex items-center justify-center gap-x-5 h-screen">
-      <section className="w-[45%]">
-        <h1 className="text-cyan-900 font-extrabold text-5xl ">
+    <main className="p-10 flex sm:flex-row flex-col items-center justify-center gap-x-5 sm:h-screen">
+      <section className="sm:w-[45%]">
+        <h1 className="text-cyan-900 font-extrabold text-2xl sm:text-5xl ">
           Navigating the digital landscape to success
         </h1>
         <p className="mt-5 font-normal text-justify text-gray-500">
@@ -19,7 +19,7 @@ const HomePage = () => {
           </button>
         </Link>
       </section>
-      <section>
+      <section className=" pt-5 sm:pt-0">
         <img
           src="https://media.istockphoto.com/id/1443585607/vector/advertising-marketing-campaign-product-promotion-using-outdoor-internet-ads-direct-marketing.jpg?s=612x612&w=0&k=20&c=MzU7Zue-rnLIQQRBHWCA4-oSt9BYhhpVzTAkPGiyJuw="
           alt=""
